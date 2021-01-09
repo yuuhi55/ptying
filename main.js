495f386df57adf1d76576796b652b31b95203bdb
優灯(ゆうひ) <68366455+yuuhi55@users.noreply.github.com>
2021-01-09 15:56:06 +0900

    関数に収納

--- main.js
+++ main.js
@@ -13,25 +13,40 @@ const button = document.getElementById("button");
 
 
 // ボタンを押すとa,b,c....の配列からランダムに出題される
+
+function ran (){
+    const y = q[Math.floor(Math.random() * q.length)];
+    return y
+    // for(i = y.length -1;i>0;i--){
+    //     //乱数生成を使ってランダムに取り出す値を決める
+    //     r = Math.floor(Math.random()*(i+1));
+    //     //取り出した値と箱の外の先頭の値を交換する
+    //     tmp = y[i];
+    //     y[i] = y[r];
+    //     y[r] = tmp;
+
+    //     return y;
+    // }
+
+
+}
+
 button.addEventListener("click", () => {
-   document.getElementById("question").textContent = q[Math.floor(Math.random() * q.length)];
-   
+   document.getElementById("question").textContent = ran();
 });
+
+
+
+
+
+
+
+
 
 
 // 上記 q[Math.floor(Math.random() * q.length) で配列からランダムに取得したものを
 // 下のフィッシャーイェーツで混ぜたい(ミルクティー → クティミールなどランダムに)
 
-for(i = q.length -1;i>0;i--){
-    //乱数生成を使ってランダムに取り出す値を決める
-    r = Math.floor(Math.random()*(i+1));
-    //取り出した値と箱の外の先頭の値を交換する
-    tmp = q[i];
-    q[i] = q[r];
-    q[r] = tmp;
-
-   
-}
 
 
 
