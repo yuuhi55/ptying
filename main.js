cdb919cde64b23764790a1213e555df65df1cd29
優灯(ゆうひ) <68366455+yuuhi55@users.noreply.github.com>
2021-01-12 17:50:10 +0900

    一旦完成

--- main.js
+++ main.js
@@ -1,62 +1,97 @@
 //問題文 現状わかりやすい単語で代用中
-a = ["ミ","ル","ク","テ","ィ","ー"].join("");
+// a = ["ミ","ル","ク","テ","ィ","ー"];
+// この時点で[].join("")記入すると下の文でシャッフルできない。理由不明
+a = "プログラム".split("");
 
+b = "アルゴリズム".split("");
 
-b = ["ペ","ッ","ト","ボ","ト","ル"].join("");
+c = "メソッド".split("");
 
+d = "かんすう".split("")
 
-c = ["エ","コ","バ","ッ","グ"].join("");
+e = "クロージャ".split("")
 
-q = [a,b,c];
+f = "コーディング".split("")
 
-const button = document.getElementById("button");
+g = "システムエンジニア".split("")
+
+h = "人工知能".split("")
+
+i = "スコープ".split("")
+
+j = "ソフトウェア".split("")
+
+k = "ソースコード".split("")
+
+l = "テキストエディタ".split("")
+
+m = "チューリングマシン".split("")
+
+n = "デバッグ".split("")
+
+o = "マークアップ言語".split("")
+
+p = "無限ループ".split("")
+
+q = "リファクタリング".split("")
+
+r = "リポジトリ".split("")
+
+s = "ワークツリー".split("")
+
+t = "インデックス".split("")
+
+u = "コミット".split("")
+
+v = "プッシュ".split("")
+
+w = "プル".split("")
+
+x = "フェッチ".split("")
+
+y = "マージ".split("")
+
+z = "ブランチ".split("")
+
+q = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
+
 
 
 // ボタンを押すとa,b,c....の配列からランダムに出題される
 
-function ran (){
-    const y = q[Math.floor(Math.random() * q.length)];
-    return y
-    // for(i = y.length -1;i>0;i--){
-    //     //乱数生成を使ってランダムに取り出す値を決める
-    //     r = Math.floor(Math.random()*(i+1));
-    //     //取り出した値と箱の外の先頭の値を交換する
-    //     tmp = y[i];
-    //     y[i] = y[r];
-    //     y[r] = tmp;
-
-    //     return y;
-    // }
-
-
+function ran(){
+    answer = [];
+    // 回答を格納
+    answer = q[Math.floor(Math.random() * q.length)];
+    // シャッフルするためにコピーする
+    const y = answer.slice()
+    for(i = y.length -1;i>0;i--){
+        r = Math.floor(Math.random()*(i+1));
+        tmp = y[i];
+        y[i] = y[r];
+        y[r] = tmp;
+        return y;
+    }
 }
+    
 
-button.addEventListener("click", () => {
-   document.getElementById("question").textContent = ran();
+
+document.getElementById("button").addEventListener("click", () => {
+   document.getElementById("question").textContent = ran().join("");
+//    最終的に表示する場面で.join("")で""を外す。
+
 });
-
-
-
-
-
-
-
-
-
-
-// 上記 q[Math.floor(Math.random() * q.length) で配列からランダムに取得したものを
-// 下のフィッシャーイェーツで混ぜたい(ミルクティー → クティミールなどランダムに)
-
-
 
 
 // 出題時の配列内文字と、入力された文字が合っているかの正誤判定をしたい。
 document.getElementById("cheak").addEventListener("click", () => {
-    if(document.getElementById("answer").value === q){
-        // 上記ｑが多分ボタンを押したときの返り値？とかになるのだと思う。
+    if(document.getElementById("answer").value === answer.join("")) {
+        // 上記a.joinが多分ボタンを押したときの返り値？とかになるのだと思う。yはスコープ外？で使えない
         window.alert("正解");
     } else {
         window.alert("不正解")
     }
 });
+
+
 
